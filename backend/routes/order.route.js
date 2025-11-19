175cde354940841698f7cf889fdbe4617bd0a988
Aastha Rao <aastharao1610@gmail.com>
2025-11-19 16:33:13 +0530

    updated import of cancel order

--- backend/routes/order.route.js
+++ backend/routes/order.route.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { verifyToken } from "../middleware/auth.js";
 import createOrder from "../controllers/orderController/createOrder.controller.js";
-import cancelOrder from "../controllers/orderController/CancelOrder.controller.js";
+import cancelOrder from "../controllers/orderController/cancelOrder.controller.js";
 import getOrder from "../controllers/orderController/getOrder.controller.js";
 import OrderStatus from "../controllers/orderController/orderStatus.controller.js";
 import getOrderByID from "../controllers/orderController/getOrserById.controller.js";
